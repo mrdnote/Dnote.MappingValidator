9993e11371274fa9875e60b1348812ad6b9a726f
Dave de Jong ♫ <dave@dnote.nl>
2022-07-28 22:02:54 +0200

    * Make excluded properties a list instead of array of strings.

--- Dnote.MappingValidator.Library/ValidateMappingAttribute.cs
+++ Dnote.MappingValidator.Library/ValidateMappingAttribute.cs
@@ -3,7 +3,7 @@
     [AttributeUsage(AttributeTargets.Class | AttributeTargets.Property | AttributeTargets.Field)]
     public class ValidateMappingAttribute : Attribute
     {
-        public string[] ExcludedProperties { get; } = new string[0];
+        public string[] ExcludedProperties { get; } = Array.Empty<string>();
 
         public ValidateMappingAttribute()
         {
